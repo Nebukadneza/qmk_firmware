909666da8e7e11dd8cf6ff844fd5f32059bbbbb8
Dario Ernst <dario@kanojo.de>
2018-02-28 17:20:50 +0100

    add all neo modifiers

--- keyboards/ergodox_infinity/keymaps/nebuneo/keymap.c
+++ keyboards/ergodox_infinity/keymaps/nebuneo/keymap.c
@@ -7,33 +7,38 @@
 // define layers
 #define BASE 0 // default layer
 
+// custom macro names
+enum functions {
+	M_KC_CAPS_L,
+	M_KC_CAPS_R
+};
+
 // define custom macros
 #define L_KC_CAPS F(M_KC_CAPS_L)
+#define R_KC_CAPS F(M_KC_CAPS_R)
 
 // timers for tap/hold
 static uint16_t m_kc_caps_l_timer;
+static uint16_t m_kc_caps_r_timer;
 
-enum functions {
-	M_KC_CAPS_L
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_TILD,        KC_1,         KC_2,   KC_3,   KC_4,       KC_5,   KC_TAB,
-        KC_NO,        KC_Q,         KC_W,   KC_E,   KC_R,         KC_T,   KC_ENTER,
-        KC_ESC,        KC_A,         KC_S,   KC_D,   L_KC_CAPS,   KC_G,
-        KC_LSFT,      KC_Z,         KC_X,   KC_C,   KC_V,   KC_B, KC_CAPS,
+        KC_TILD,        KC_1,         KC_2,   KC_3,   KC_4,         KC_5,         KC_TAB,
+        KC_NO,          KC_Q,         KC_W,   KC_E,   KC_R,         KC_T,         KC_ENTER,
+        KC_ESC,         KC_A,         KC_S,   KC_D,   L_KC_CAPS,    ALGR_T(KC_G),
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,         KC_B,         KC_CAPS,
            KC_NO,KC_NO,KC_NO,KC_NO,KC_LGUI,
                                                         KC_NO, KC_NO,
                                                                KC_NO,
                                                KC_SPC,KC_CAPS,KC_RALT,
         // right hand
-             KC_TAB,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             KC_ENTER,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             DE_UE,
-                          KC_H,   KC_J,  KC_K,   KC_L,   DE_OE,            DE_AE,
-             KC_CAPS,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,   KC_RSHIFT,
-                                             KC_RGUI,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
+             KC_TAB,      KC_6,            KC_7,       KC_8,   KC_9,   KC_0,             KC_MINS,
+             KC_ENTER,    KC_Y,            KC_U,       KC_I,   KC_O,   KC_P,             DE_UE,
+                          ALGR_T(KC_H),    R_KC_CAPS,  KC_K,   KC_L,   DE_OE,            DE_AE,
+             KC_CAPS,     KC_N,            KC_M,       KC_COMM,KC_DOT, KC_SLSH,          KC_RSHIFT,
+                                                        KC_RGUI,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
              KC_NO,KC_NO,
              KC_NO,
              KC_RALT,KC_RCTL, KC_BSPC
@@ -42,6 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
 	[M_KC_CAPS_L] = ACTION_MACRO_TAP(M_KC_CAPS_L),
+	[M_KC_CAPS_R] = ACTION_MACRO_TAP(M_KC_CAPS_R),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -60,6 +66,22 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 				if (tap_not_interrupted && timer_elapsed(m_kc_caps_l_timer) < TAPPING_TERM) { // send tap-character
 					record->tap.count = 0;
 					return MACRO(T(F), END);
+				} else { // release mod
+					return MACRO(U(CAPS), END);
+				}
+			}
+		break;
+		case M_KC_CAPS_R:
+			if (record->event.pressed) {
+				m_kc_caps_r_timer = timer_read();
+
+				if (!tap_not_interrupted) { //set mod
+					return MACRO(D(CAPS), END);
+				}
+			} else {
+				if (tap_not_interrupted && timer_elapsed(m_kc_caps_r_timer) < TAPPING_TERM) { // send tap-character
+					record->tap.count = 0;
+					return MACRO(T(J), END);
 				} else { // release mod
 					return MACRO(U(CAPS), END);
 				}
