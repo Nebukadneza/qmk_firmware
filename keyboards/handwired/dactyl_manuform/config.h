af954b30441af6c25f689df03c1f8fcf9f771235
Dario Ernst <daddel9@nebuk.de>
2018-07-13 18:33:14 +0200

    initial adaptation try

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -31,13 +31,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 // Rows are doubled-up
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 5
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 7
 
 // wiring of each half
-#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
-// #define MATRIX_COL_PINS { B5, B4, E6, D7, C6 }
-#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
+// right
+//#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+//#define MATRIX_COL_PINS { D1, D4, C6, D7, E6, B4, B5 }
+// left
+#define MATRIX_ROW_PINS { B6, B2, B3, B1, F7, F6 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4, D1 }
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -1,129 +1,43 @@
 #include "dactyl_manuform.h"
 #include "action_layer.h"
 #include "eeconfig.h"
+#include "keymap_german.h"
 
 extern keymap_config_t keymap_config;
 
-#define _BASE 0
-#define _RAISE 1
-#define _LOWER 2
+#define BASE 0 // default layer
+#define RALGR_LHOLD 1 // When pressing right ALGR, unmap left not to mask cursors
 
-// Fillers to make layering more clear
-
-#define ____ KC_TRNS
-
-#define SFT_ESC  SFT_T(KC_ESC)
-#define CTL_BSPC CTL_T(KC_BSPC)
-#define ALT_SPC  ALT_T(KC_SPC)
-#define SFT_ENT  SFT_T(KC_ENT)
-
-#define KC_ML KC_MS_LEFT
-#define KC_MR KC_MS_RIGHT
-#define KC_MU KC_MS_UP
-#define KC_MD KC_MS_DOWN
-#define KC_MB1 KC_MS_BTN1
-#define KC_MB2 KC_MS_BTN1
-
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+#define NB_D ALGR_T(KC_D)
+#define NB_F RGUI_T(KC_F)
+#define NB_J RGUI_T(KC_J)
+#define NB_K LT(RALGR_LHOLD, KC_K)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-/* Base (qwerty)
- * ,----------------------------------,                             ,----------------------------------,
- * |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |
- * |------+------+------+------+------|                             |-------------+------+------+------|
- * |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |
- * |------+------+------+------+------|                             |------|------+------+------+------|
- * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   '  |
- * |------+------+------+-------------,                             ,-------------+------+------+------,
- *        |  [   |   ]  |                                                         |   -  |   =  |
- *        '------+------'-------------'                             '-------------'------+------'
- *                      | ESC  |  BS  |                             | SPACE|ENTER |
- *                      |  +   |   +  |                             |  +   |  +   |
- *                      | SHIFT| CTRL |                             | ALT  |SHIFT |
- *                      '------+------'                             '------+------'
- *                                    '------+------' '------+------'
- *                                    | TAB  | HOME | | END  | DEL  |
- *                                    '------+------' '------+------'
- *                                    | Lower|  ~   | | GUI  | Raise|
- *                                    '------+------' '------+------'
- */
-
-[_BASE] = LAYOUT( \
-  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    \
-  KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, \
-  KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, \
-              KC_LBRC, KC_RBRC,                                                   KC_MINS, KC_EQL,                   \
-                                      SFT_ESC, CTL_BSPC,  ALT_SPC, SFT_ENT,                                          \
-                                      KC_TAB,  KC_HOME,   KC_END,  KC_DEL,                                           \
-                                      RAISE,   KC_GRV,    KC_LGUI, LOWER
+[BASE] = LAYOUT( \
+	KC_TILD, KC_1, KC_2, KC_3,   KC_4, KC_5,   KC_6,                             KC_7,    KC_8,   KC_9,    KC_0,   KC_MINS,   KC_NO,      KC_PAUSE, \
+	DEBUG,  KC_ESC, KC_Q, KC_W,   KC_E, KC_R,   KC_T,                             KC_Z,    KC_U,   KC_I,    KC_O,   KC_P,      DE_UE,     DE_PLUS, \
+	KC_UP,  KC_LSFT, KC_A, KC_S,   NB_D, NB_F,   KC_G,                             KC_H,    NB_J,   NB_K,    KC_L,   DE_OE,     DE_AE,    DE_HASH, \
+	KC_DOWN, KC_NO, KC_Y, KC_X,   KC_C, KC_V,   KC_B,                             KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RSHIFT,   KC_NO, \
+	                     KC_LEFT,   KC_RIGHT,                                                            KC_PGUP,    KC_PGDOWN,                  \
+	                                   KC_SPC, KC_RCTL,                          KC_RCTL, KC_SPC,                        \
+	                                           KC_ENTER, KC_BSPC,       KC_BSPC,   KC_ENTER,                              \
+	                                           KC_TAB,  KC_LALT,      KC_LALT,  KC_TAB                               \
 ),
-
-/* Raise
- * ,----------------------------------,                             ,----------------------------------,
- * |      |      |  mup |      |      |                             | VOL+ |      |  up  |      | PgUp |
- * |------+------+------+------+------|                             |-------------+------+------+------|
- * |      | mleft| mdown|mright|      |                             | MUTE | left | down |right | PgDn |
- * |------+------+------+------+------|                             |------|------+------+------+------|
- * |      |      |      |      |      |                             | VOL- |  /   |  \   |  ?   |  |   |
- * |------+------+------+-------------,                             ,-------------+------+------+------,
- *        |      |      |                                                         | mbtn |mbtn2 |
- *        '------+------'-------------'                             '-------------'------+------'
- *                      |      |      |                             |      |      |
- *                      |      |      |                             |      |      |
- *                      |      |      |                             |      |      |
- *                      '------+------'                             '------+------'
- *                                    '------+------' '------+------'
- *                                    |      |      | |      |      |
- *                                    '------+------' '------+------'
- *                                    |      |      | |      |      |
- *                                    '------+------' '------+------'
- */
-
-[_RAISE] = LAYOUT( \
-  ____, ____,  KC_MU, ____,  ____,                          KC_VOLU, ____,    KC_UP,   ____,     KC_PGUP,   \
-  ____, KC_ML, KC_MD, KC_MR, ____,                          KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, \
-  ____, ____,  ____,  ____,  ____,                          KC_VOLD, KC_SLSH, KC_BSLS, KC_QUES,  KC_PIPE,   \
-        ____,  ____,                                                          KC_MB1,  KC_MB2,              \
-                                   ____, ____,  ____, ____,                                                 \
-                                   ____, ____,  ____, ____,                                                 \
-                                   ____, ____,  ____, ____                                                  \
-),
-/* Lower
- * ,----------------------------------,                             ,----------------------------------,
- * | F1   | F2   | F3   | F4   | F5   |                             |  F6  | F7   |  F8  |  F9  |  F10 |
- * |------+------+------+------+------|                             |-------------+------+------+------|
- * |  1   |  2   |  3   |  4   |  5   |                             |  6   |  7   |  8   |  9   |  10  |
- * |------+------+------+------+------|                             |------|------+------+------+------|
- * |  !   |  @   |  #   |  $   |  %   |                             |  ^   |  &   |  *   |  (   |  )   |
- * |------+------+------+-------------,                             ,-------------+------+------+------,
- *        | F11  | F12  |                                                         |  -   |  =   |
- *        '------+------'-------------'                             '-------------'------+------'
- *                      |      |      |                             |      |      |
- *                      |      |      |                             |      |      |
- *                      |      |      |                             |      |      |
- *                      '------+------'                             '------+------'
- *                                    '------+------' '------+------'
- *                                    |      |      | |      |      |
- *                                    '------+------' '------+------'
- *                                    |      |      | |      |      |
- *                                    '------+------' '------+------'
- */
-
-[_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  \
-  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    \
-  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                          KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, \
-           KC_F11, KC_F12,                                                               ____,    ____,             \
-                                             ____, ____,  ____, ____,                                               \
-                                             ____, ____,  ____, ____,                                               \
-                                             ____, ____,  ____, ____                                                \
+[RALGR_LHOLD] = LAYOUT( \
+	RALT(KC_TILD), RALT(KC_1), RALT(KC_2), RALT(KC_3),   RALT(KC_4), RALT(KC_5),   RALT(KC_6),                             KC_7,    KC_8,   KC_9,    KC_0,   KC_MINS,   KC_2,   KC_PAUSE, \
+	RALT(KC_TAB),  RALT(KC_5), RALT(KC_Q), RALT(KC_W),   RALT(KC_E), RALT(KC_R),   RALT(KC_T),                             KC_Z,    KC_U,   KC_I,    KC_O,   KC_P,      DE_UE,  KC_6, \
+	RALT(KC_ESC),  RALT(KC_6), RALT(KC_A), RALT(KC_S),   RALT(KC_D), RALT(KC_F),   RALT(KC_G),                             KC_H,    NB_J,   NB_K,    KC_L,   DE_OE,     DE_AE,  KC_7, \
+	RALT(KC_LSFT), RALT(KC_7), RALT(KC_Y), RALT(KC_X),   RALT(KC_C), RALT(KC_V),   RALT(KC_B),                             KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_9,   KC_RSHIFT, \
+	                     RALT(KC_5),   RALT(KC_4),                                                                                          KC_2,    KC_5,                  \
+	                             RALT(KC_SPC), KC_RCTL,                                                                     KC_RCTL, KC_SPC,                        \
+	                                           KC_ENTER, KC_BSPC,                                                  KC_BSPC,   KC_ENTER,                              \
+	                                           KC_TAB,  KC_LALT,                                                 KC_LALT,  KC_TAB                               \
 )
 };
 
-void persistent_default_layer_set(uint16_t default_layer) {
+/*void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
-}
+}*/
 
