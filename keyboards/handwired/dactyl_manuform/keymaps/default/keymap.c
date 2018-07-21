f5b255f725eb820a09b8abfea13c2468d2ee06f5
Your Name <you@example.com>
2018-07-21 16:13:46 +0200

    different shift modtap

--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -23,19 +23,21 @@ extern keymap_config_t keymap_config;
 #define NB_AE SFT_T(DE_AE)
 #define NB_A LT(RHASH, KC_A)
 #define NB_OE LT(LHASH, DE_OE)
+#define NB_S SFT_T(KC_S)
+#define NB_L SFT_T(KC_L)
 
 //static uint32_t last_layer_state = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT( \
-	LT(FX, KC_TILD),   KC_1,   KC_2,      KC_3,   KC_4, KC_5,   KC_6,                        KC_7,    KC_8,   KC_9,    KC_0,   KC_MINS,   LT(FX, DE_ACUT),       KC_PAUSE, \
-	       KC_PSCR,  KC_ESC,  KC_Q,      KC_W,   KC_E, KC_R,   KC_T,                         KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,      DE_UE,         DE_PLUS, \
-	       KC_UP,     KC_LSFT, NB_A, KC_S,   NB_D, NB_F,   KC_G,                             KC_H,    NB_J,   NB_K,    KC_L,   NB_OE,     NB_AE,    DE_HASH, \
+	LT(FX, KC_TILD),   KC_1,   KC_2,      KC_3,   KC_4, KC_5,   KC_6,                        KC_7,    KC_8,   KC_9,    KC_0,   KC_MINS,   DE_ACUT,       LT(FX, KC_PAUSE), \
+	       KC_PSCR,   KC_ESC,  KC_Q,      KC_W,   KC_E, KC_R,   KC_T,                         KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,      DE_UE,         DE_PLUS, \
+	       KC_UP,     KC_LSFT, NB_A,     NB_S,   NB_D, NB_F,   KC_G,                             KC_H,    NB_J,   NB_K,    NB_L,   NB_OE,     NB_AE,    DE_HASH, \
 	       KC_DOWN,   DE_HASH,  KC_Z,      KC_X,   KC_C, KC_V,   KC_B,                       KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RSHIFT,   KC_SLCK, \
 	                                      KC_LEFT,   KC_RIGHT,                                     KC_PGUP,    KC_PGDOWN,                  \
 	                                            KC_SPC, KC_RCTL,                          KC_RCTL, KC_SPC,                        \
-	                                                    KC_ENTER, KC_BSPC,     KC_BSPC,   KC_ENTER,                              \
-	                                                    KC_TAB,  KC_LALT,      KC_LALT,  KC_TAB                               \
+	                                                    KC_TAB, KC_BSPC,     KC_BSPC,   KC_TAB,                              \
+	                                                    KC_ENTER,  KC_LALT,      KC_LALT,  KC_ENTER                               \
 ),
 [RALGR_LHOLD] = LAYOUT( \
 	RALT(KC_TILD),  RALT(KC_1),    RALT(KC_2), RALT(KC_3),   RALT(KC_4), RALT(KC_5),   RALT(KC_6),                             KC_7,    KC_8,   KC_9,    KC_0,   KC_MINS,   KC_2,   KC_PAUSE, \
